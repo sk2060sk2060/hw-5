ad408b2e6f524487275580cfb2eb4ac4f47b3f91
sk <sk2060sk2060@gmail.com>
2020-01-19 14:27:30 -0700

    Fixed typo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var idsCollection = ["#9", "#10", "#11", "#12", "#1", "#2", "#3", "#4",  "#5"];
 var timeSlotCollection = ["09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00",  "14:00:00",  "15:00:00",  "16:00:00",  "17:00:00"];
-var shiftedTimeSlotCollection = ["10:00:00", "11:00:00", "12:00:00", "13:00:00",  "14:00:00",  "15:00:00",  "16:00:00",  "17:00:00",  "17:00:00"];
+var shiftedTimeSlotCollection = ["10:00:00", "11:00:00", "12:00:00", "13:00:00",  "14:00:00",  "15:00:00",  "16:00:00",  "17:00:00",  "18:00:00"];
 
 var  plannerContent = [];
 var getLocalStorageData = JSON.parse(localStorage.getItem("planner-items"));
